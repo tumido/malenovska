d1aa36e8ec3ac6303d161e9d64b09dc5185261f6
Tomas Coufal <tcoufal@redhat.com>
2018-09-29 18:47:27 +0200

    Cleanup Footer

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -3,7 +3,12 @@ import './style.scss';
 
 const Footer = () => (
   <footer>
-    <section>a</section>
+    <section>
+      Zlišky <span role="img" area-label="heart-emoji">❤️</span> Malenovská
+    </section>
+    <section>
+      <a href="https://reactjs.org/">React</a>, <a href="https://redux.js.org/">Redux</a>, <a href="https://firebase.google.com/docs/firestore/">Google Cloud Firestore</a>
+    </section>
   </footer>
 );
 
