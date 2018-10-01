bd046b9cc55d54461ad9937b9127f584c8643b72
Tomas Coufal <tcoufal@redhat.com>
2018-10-01 10:27:03 +0200

    Fix Redux dispatch pass to Header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,40 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { push as Menu } from 'react-burger-menu';
-import {decorator as reduxBurgerMenu} from 'redux-burger-menu';
+import {decorator as reduxBurgerMenu, action as toggleMenu} from 'redux-burger-menu';
 import Logo from 'components/Logo';
 import './style.scss';
 
-class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <Menu
-        pageWrapId={ "app-content" }
-        outerContainerId={ "app-wrapper" }
-        customBurgerIcon={
-          <span>
-            <span className="bm-burger-bars"></span><span className="bm-burger-bars"></span><span className="bm-burger-bars"></span>
-          </span>
-        }
-      >
-        <Link to="/" className="bm-item__logo">
-          <Logo />
-        </Link>
-        <Link className="router-link title" to="/bitva/legends">
-          <i class="fas fa-book-open"></i>Zprávy z bojiště
-        </Link>
-        <Link className="router-link title" to="/bitva/rules">
-          <i class="fas fa-balance-scale"></i> Pravidla střetu
-        </Link>
-        <Link className="router-link title" to="/bitva/info">
-          <i class="fas fa-map-marker-alt"></i> Důležité informace
-        </Link>
-        <Link className="router-link title bm-item--highlight" to="/bitva/registration">
-          <i class="fas fa-address-card"></i>Registrace
-        </Link>
-      </Menu>
-    );
-  }
+const Header = props => {
+  return (
+    <Menu
+      pageWrapId={ "app-content" }
+      outerContainerId={ "app-wrapper" }
+      customBurgerIcon={
+        <span>
+          <span className="bm-burger-bars"></span><span className="bm-burger-bars"></span><span className="bm-burger-bars"></span>
+        </span>
+      }
+      {...props}
+    >
+      <Link onClick={() => {props.toggleMenu(false)}} to="/" className="bm-item__logo">
+        <Logo />
+      </Link>
+      <Link onClick={() => {props.toggleMenu(false)}} className="router-link title" to="/bitva/legends">
+        <i className="fas fa-book-open"></i>Zprávy z bojiště
+      </Link>
+      <Link onClick={() => {props.toggleMenu(false)}} className="router-link title" to="/bitva/rules">
+        <i className="fas fa-balance-scale"></i> Pravidla střetu
+      </Link>
+      <Link onClick={() => {props.toggleMenu(false)}} className="router-link title" to="/bitva/info">
+        <i className="fas fa-map-marker-alt"></i> Důležité informace
+      </Link>
+      <Link onClick={() => {props.toggleMenu(false)}} className="router-link title bm-item--highlight" to="/bitva/registration">
+        <i className="fas fa-address-card"></i>Registrace
+      </Link>
+    </Menu>
+  )
+};
+
+const mapDispatchToProps = {
+  toggleMenu,
 }
 
-export default reduxBurgerMenu(Header);
+export default connect(null, mapDispatchToProps)(reduxBurgerMenu(Header));
