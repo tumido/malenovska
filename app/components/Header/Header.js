4667c7f17921b2df74fa6e09edee2025569d3380
Tomas Coufal <tcoufal@redhat.com>
2019-03-29 02:29:31 +0100

    New header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,42 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Menu from 'react-burger-menu/lib/menus/push';
 import {decorator as reduxBurgerMenu, action as toggleMenu} from 'redux-burger-menu';
 import Logo from 'components/Logo';
 import './style.scss';
 
 const Header = props => {
+  const base = props.location.pathname.split('/', 2).join('/')
+  const close = () => {props.toggleMenu(false)}
+
   return (
-    <Menu
-      pageWrapId={ "app-content" }
-      outerContainerId={ "app-wrapper" }
-      customBurgerIcon={
-        <span>
-          <span className="bm-burger-bars"></span><span className="bm-burger-bars"></span><span className="bm-burger-bars"></span>
-        </span>
-      }
-      {...props}
-    >
-      <Link onClick={() => {props.toggleMenu(false)}} to="/" className="bm-item__logo">
-        <Logo />
-      </Link>
-      <Link onClick={() => {props.toggleMenu(false)}} className="router-link custom-font" to="/">
-        <i className="fas fa-home"></i>Úvodní stránka
-      </Link>
-      <Link onClick={() => {props.toggleMenu(false)}} className="router-link custom-font" to="./legends">
-        <i className="fas fa-book-open"></i>Zprávy z bojiště
-      </Link>
-      <Link onClick={() => {props.toggleMenu(false)}} className="router-link custom-font" to="./rules">
-        <i className="fas fa-balance-scale"></i> Pravidla střetu
-      </Link>
-      <Link onClick={() => {props.toggleMenu(false)}} className="router-link custom-font" to="./info">
-        <i className="fas fa-map-marker-alt"></i> Důležité informace
-      </Link>
-      <Link onClick={() => {props.toggleMenu(false)}} className="router-link custom-font bm-item--highlight" to="./registration">
-        <i className="fas fa-address-card"></i>Registrace
-      </Link>
-    </Menu>
+    <div>
+      <Menu
+        pageWrapId={ "app-content" }
+        outerContainerId={ "app-wrapper" }
+        customBurgerIcon={
+          <span>
+            <span className="bm-burger-bars"></span><span className="bm-burger-bars"></span><span className="bm-burger-bars"></span>
+          </span>
+        }
+        {...props}
+      >
+        <NavLink onClick={close} to="/" className="bm-item__logo">
+          <Logo />
+        </NavLink>
+        <NavLink onClick={close} className="router-link custom-font" to={base + "/legends"} activeClassName="bm-item--highlight">
+          <i className="fas fa-book-open"></i>Zprávy z bojiště
+        </NavLink>
+        <NavLink onClick={close} className="router-link custom-font" to={base + "/rules"} activeClassName="bm-item--highlight">
+          <i className="fas fa-balance-scale"></i> Pravidla střetu
+        </NavLink>
+        <NavLink onClick={close} className="router-link custom-font" to={base + "/info"} activeClassName="bm-item--highlight">
+          <i className="fas fa-map-marker-alt"></i> Důležité informace
+        </NavLink>
+        <NavLink onClick={close} className="router-link custom-font" to={base + "/registration"} activeClassName="bm-item--highlight">
+          <i className="fas fa-address-card"></i>Registrace
+        </NavLink>
+        <NavLink onClick={close} className="router-link custom-font margin-top" to="/">
+          <i className="fas fa-map-signs"></i>Úvodní stránka
+        </NavLink>
+      </Menu>
+    </div>
   )
 };
 
