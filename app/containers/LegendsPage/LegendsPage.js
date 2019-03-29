b7e549c088534aed88cfd5eec74b398602fd6f5f
Tomas Coufal <tcoufal@redhat.com>
2019-03-29 01:39:36 +0100

    Dynamic legends

--- app/containers/LegendsPage/LegendsPage.js
+++ app/containers/LegendsPage/LegendsPage.js
@@ -11,6 +11,7 @@ import { firestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 
 import Article from 'components/Article';
 import LoadingIndicator from 'components/LoadingIndicator';
+import get from 'lodash/get'
 
 import './style.scss';
 
@@ -18,7 +19,7 @@ const LegendsPage = ({ legends }) => {
   const legendsList = !isLoaded(legends)
     ? <LoadingIndicator />
     : isEmpty(legends)
-      ? ''
+      ? <Article id="legend-not-found" title='Néni nic!' content='Bohužel nemáme co nabídnout'/>
       : Object.keys(legends).map(
         (key, id) => (
           <Article
@@ -37,13 +38,15 @@ const LegendsPage = ({ legends }) => {
 }
 
 export default compose(
-  firestoreConnect(({event}) => [
+  firestoreConnect(({location}) => [
     {
       collection: "legends",
+      where: ['event', '==', location.pathname.split('/', 2)[1]],
+      storeAs: `legends_${location.pathname.split('/', 2)[1]}`
       // collection: "legends",
     }
   ]),
-  connect((state) => ({
-    legends: state.firestore.data.legends
+  connect((state, props) => ({
+    legends: get(state.firestore.data, `legends_${props.location.pathname.split('/', 2)[1]}`)
   }))
 )(LegendsPage);
