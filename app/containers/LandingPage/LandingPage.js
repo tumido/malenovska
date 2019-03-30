caf49c4a0233f863ff885d28ee0a4ca3eea03ff5
Tomas Coufal <tcoufal@redhat.com>
2019-03-30 01:11:08 +0100

    Responsiveness update

--- app/containers/LandingPage/LandingPage.js
+++ app/containers/LandingPage/LandingPage.js
@@ -5,26 +5,24 @@ import './style.scss';
 import Logo from 'components/Logo';
 
 const LandingPage = () => (
-  <div className="landing-page">
+  <nav className="LandingPage">
     <Helmet>
       <meta name="description" content="Vyber si Malenovskou, která tě zajímá" />
     </Helmet>
-    <nav className="main">
-      <div className="panel">
-        <Link className="panel-content router-link custom-font effect-lighter" to="/sarvatka/legends">Šarvátka</Link>
-      </div>
+    <div className="panel panel--side">
+      <Link className="panel-content router-link custom-font effect-lighter" to="/sarvatka/legends">Šarvátka</Link>
+    </div>
 
-      <div className="panel logo-panel">
-        <div className="panel-content logo">
-          <Logo />
-        </div>
+    <div className="panel panel--center">
+      <div className="panel-content logo">
+        <Logo />
       </div>
+    </div>
 
-      <div className="panel">
-        <Link className="panel-content router-link custom-font effect-darker" to="/bitva/legends">Bitva</Link>
-      </div>
-    </nav>
-  </div>
+    <div className="panel panel--side">
+      <Link className="panel-content router-link custom-font effect-darker" to="/bitva/legends">Bitva</Link>
+    </div>
+  </nav>
 );
 
 export default LandingPage;
