0580f17264777ffc179f23d3f632371a5a0b1de6
Tomas Coufal <tcoufal@redhat.com>
2019-09-13 21:50:46 +0200

    Add ScrollTop

---
+++ src/components/ScrollTop/ScrollTop.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles, Zoom, Icon, Fab, useScrollTrigger, Tooltip } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
+
+const ScrollTop = ({ anchor }) => {
+  const classes = useStyles();
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100
+  });
+
+  const handleClick = event => {
+    const curAnchor = (event.target.ownerDocument || document).querySelector(anchor);
+
+    if (curAnchor) {
+      curAnchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  };
+
+  return (
+    <Zoom in={ trigger }>
+      <div onClick={ handleClick } role="presentation" className={classes.root}>
+        <Tooltip title="Na začátek stránky"  aria-label="Scroll back to top" placement='left'>
+          <Fab color="secondary" size="medium">
+            <Icon>keyboard_arrow_up</Icon>
+          </Fab>
+        </Tooltip>
+      </div>
+    </Zoom>
+  );
+}
+
+ScrollTop.propTypes = {
+  anchor: PropTypes.string
+}
+
+export default ScrollTop;
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import ArticlePreview from './ArticlePreview';
-import Header from './Header';
 import Footer from './Footer';
+import Header from './Header';
 import Loading from './Loading';
+import ScrollTop from './ScrollTop';
 
 export {
   ArticlePreview,
-  Header,
   Footer,
-  Loading
+  Header,
+  Loading,
+  ScrollTop
 };
