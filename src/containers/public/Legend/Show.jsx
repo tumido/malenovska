aaa8a13d5d85251caf499f291fc9410eff7d2bad
Tomas Coufal <tcoufal@redhat.com>
2019-09-21 14:09:50 +0200

    Fix legend loading

--- src/containers/public/Legend/Show.jsx
+++ src/containers/public/Legend/Show.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { isLoaded, useFirestoreConnect } from 'react-redux-firebase';
 import PropTypes from 'prop-types';
 import { Typography, Paper, Container, Chip } from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
 import { makeStyles } from '@material-ui/core/styles';
 
-import { Loading, ScrollTop, Markdown } from 'components';
+import { ScrollTop, Markdown } from 'components';
 import { timestampToDateStr } from 'utilities/firebase';
 
 const useStyles = makeStyles(theme => ({
@@ -29,22 +30,37 @@ const useStyles = makeStyles(theme => ({
 const Show = ({ match: { params: { id }}, event }) => {
   const classes = useStyles();
 
+  console.log(useSelector(({firebase}) => firebase));
+
   useFirestoreConnect(() => ([
     {
       collection: 'legends',
       doc: id,
-      storeAs: 'legend'
+      storeAs: id
     }
   ]));
-  const legend = useSelector(({ firestore }) => firestore.ordered.legend && firestore.ordered.legend[0]);
+  const legend = useSelector(({ firestore }) => firestore.ordered[id]);
 
   if (!isLoaded(legend)) {
-    return <Loading />;
-  }
+    return (
+      <React.Fragment>
+        <Container fixed maxWidth="lg" className={ classes.root }>
+          <Paper>
+            <div className={ classes.content }>
+              <Skeleton type='rect' height={ 100 } />
+              <Skeleton type='text' height={ 24 } />
+              <Skeleton type='text' />
+              <Skeleton type='text' />
+              <Skeleton type='text' />
+            </div>
+          </Paper>
+        </Container>
+      </React.Fragment>
+    );
+  };
 
-  if (legend.event !== event.id) {
-    return 'Tato legenda tu neni'; // FIX-ME po prepsani NotFoundPage
-
+  if (!legend.length || legend[0].event !== event.id) {
+    return <Redirect to='/not-found' />
   }
 
   return (
@@ -52,10 +68,10 @@ const Show = ({ match: { params: { id }}, event }) => {
       <Container fixed maxWidth="lg" className={ classes.root }>
         <Paper>
           <div className={ classes.content }>
-            <Typography gutterBottom variant='h4' component='h1' id='top'>{ legend.title }</Typography>
+            <Typography gutterBottom variant='h4' component='h1' id='top'>{ legend[0].title }</Typography>
             <Chip label={ event.name } variant='outlined' className={ classes.chip } to={ `/${event.id}` } component={ Link } clickable/>
-            { legend.date && <Chip label={ timestampToDateStr(legend.date) } variant='outlined' className={ classes.chip }/> }
-            <Markdown content={ legend.content } />
+            { legend[0].date && <Chip label={ timestampToDateStr(legend[0].date) } variant='outlined' className={ classes.chip }/> }
+            <Markdown content={ legend[0].content } />
           </div>
         </Paper>
       </Container>
