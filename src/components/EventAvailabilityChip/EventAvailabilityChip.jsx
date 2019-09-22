d554da2a5b9ade9e06303cbd5d3ada10d608c98a
Tomas Coufal <tcoufal@redhat.com>
2019-09-22 14:17:07 +0200

    Fix prod build Avatar in Chip

--- src/components/EventAvailabilityChip/EventAvailabilityChip.jsx
+++ src/components/EventAvailabilityChip/EventAvailabilityChip.jsx
@@ -2,21 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Chip, Icon, Avatar } from '@material-ui/core';
+import { withStyles } from '@material-ui/styles';
 
-const EventAvailabilityChip = ({ event, className, color = 'primary' }) => (
+// FIXME: remove when released MUI with https://github.com/mui-org/material-ui/pull/17469
+import clsx from 'clsx';
+
+const styles = {
+  avatar: {
+    height: 32,
+    width: 32
+  },
+  avatarPrimary: {
+    color: 'rgba(0, 0, 0, 0.87)',
+    backgroundColor: 'rgb(175, 175, 175)'
+  },
+  avatarSecondary: {
+    color: '#fff',
+    backgroundColor: '#dd2c00'
+  }
+};
+
+const EventAvailabilityChip = ({ event, className, classes }) => (
   (!event.times || !event.times.date || event.times.date.toDate() < new Date()) ? (
     <Chip
       className={ className }
-      color={ color }
+      color='primary'
       label='Proběhlo'
-      avatar={ <Avatar><Icon>hourglass_empty</Icon></Avatar> }
+      avatar={ <Avatar className={ clsx(`${classes.avatar} ${classes.avatarPrimary}`) }><Icon>hourglass_empty</Icon></Avatar> }
     />
   ) : (
     <Chip
       className={ className }
       label='Nová akce'
-      color={ color === 'primary' ? 'secondary' : 'primary' }
-      avatar={ <Avatar><Icon>favorite_border</Icon></Avatar> }
+      color='secondary'
+      avatar={ <Avatar className={ clsx(`${classes.avatar} ${classes.avatarSecondary}`) }><Icon>favorite_border</Icon></Avatar> }
     />
   )
 );
@@ -24,7 +43,7 @@ const EventAvailabilityChip = ({ event, className, color = 'primary' }) => (
 EventAvailabilityChip.propTypes = {
   event: PropTypes.object.isRequired,
   className: PropTypes.string,
-  color: PropTypes.oneOf([ 'primary', 'secondary' ])
+  classes: PropTypes.object.isRequired
 };
 
-export default EventAvailabilityChip;
+export default withStyles(styles)(EventAvailabilityChip);
