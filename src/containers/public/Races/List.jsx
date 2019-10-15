5e19a4b901fa3cdce252f2a68a58086e3a703a43
Tomas Coufal <tcoufal@redhat.com>
2019-10-15 23:52:24 +0200

    Update Races list view

--- src/containers/public/Races/List.jsx
+++ src/containers/public/Races/List.jsx
@@ -3,27 +3,24 @@ import PropTypes from 'prop-types';
 import { connect, useSelector } from 'react-redux';
 import { useFirestoreConnect, isLoaded } from 'react-redux-firebase';
 
-import { Divider, Typography, Tabs, Tab, Hidden } from '@material-ui/core';
+import { Typography, Tabs, Tab, Hidden, AppBar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
 import { Article, ArticleContent, ArticleMedia, ColorBadge, Markdown } from 'components';
+import { darkTheme } from 'utilities/theme';
 
 const useStyles = makeStyles(theme => ({
   tabPanel: {
-    paddingBottom: theme.spacing(2)
-  },
-  divider: {
-    margin: '20px 0'
+    paddingBottom: theme.spacing(2),
+    paddingTop: theme.spacing(2)
   },
   wrapper: {
-    position: 'relative',
-    '& > :first-child': {
-      marginTop: 0
-    }
+    position: 'relative'
   },
   badge: {
     position: 'absolute',
-    top: theme.spacing(2),
+    top: `calc(${theme.spacing(2)}px + 2em)`,
     right: theme.spacing(2)
   }
 }));
@@ -37,17 +34,15 @@ const TabPanel = ({ race, activeTab, index }) => {
       hidden={ activeTab !== index }
       className={ classes.tabPanel }
     >
+      <ArticleContent>
+        <Typography gutterBottom variant='h4' component='h2'>{ race.name }</Typography>
+        <Markdown content={ race.requirements } />
+      </ArticleContent>
       <div className={ classes.wrapper }>
         <ArticleMedia src={ race.image && race.image.src } />
         <ColorBadge color={ race.color } className={ classes.badge } />
       </div>
       <ArticleContent>
-        <Typography gutterBottom variant='h4' component='h2'>{ race.name }</Typography>
-        <Markdown content={ race.requirements } />
-      </ArticleContent>
-      <Divider className={ classes.divider }/>
-      <ArticleContent>
-        <Typography gutterBottom variant='h5' component='h3'>Příběh</Typography>
         <Markdown content={ race.legend } />
       </ArticleContent>
     </div>
@@ -87,29 +82,33 @@ const List = ({ event }) => {
 
   return (
     <Article spacing={ 0 }>
-      <Hidden mdUp>
-        <Tabs
-          value={ activeTab }
-          onChange={ handleTabChange }
-          indicatorColor="secondary"
-          textColor="primary"
-          variant="scrollable"
-          scrollButtons="auto"
-        >
-          { tabs }
-        </Tabs>
-      </Hidden>
-      <Hidden smDown>
-        <Tabs
-          value={ activeTab }
-          onChange={ handleTabChange }
-          indicatorColor="secondary"
-          textColor="primary"
-          variant="fullWidth"
-        >
-          { tabs }
-        </Tabs>
-      </Hidden>
+      <ThemeProvider theme={ darkTheme }>
+        <AppBar position="static" color="default">
+          <Hidden mdUp>
+            <Tabs
+              value={ activeTab }
+              onChange={ handleTabChange }
+              indicatorColor="secondary"
+              textColor="secondary"
+              variant="scrollable"
+              scrollButtons="auto"
+            >
+              { tabs }
+            </Tabs>
+          </Hidden>
+          <Hidden smDown>
+            <Tabs
+              value={ activeTab }
+              onChange={ handleTabChange }
+              indicatorColor="secondary"
+              textColor="secondary"
+              variant="fullWidth"
+            >
+              { tabs }
+            </Tabs>
+          </Hidden>
+        </AppBar>
+      </ThemeProvider>
       { races.map((race, idx) => (
         <TabPanel key={ race.id } race={ race } activeTab={ activeTab } index={ idx } />
       ))}
