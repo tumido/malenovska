26c35368ae52882656887f7706dbb322a6375382
Tomas Coufal <tcoufal@redhat.com>
2020-02-22 00:05:41 +0100

    👨‍🌾 Races Admin layout updated

--- apps/admin/src/containers/Race/Edit.jsx
+++ apps/admin/src/containers/Race/Edit.jsx
@@ -10,23 +10,29 @@ import { ColorInput } from 'react-admin-color-input';
 
 import MarkdownInput from 'components/MarkdownInput';
 import { RaceTitle } from './shared';
+import { useStyles } from '../shared';
 
-const Edit = (props) => (
-  <EditBase title={ <RaceTitle /> } { ...props }>
-    <SimpleForm>
-      <TextInput label='Název' source="name" validate={ required() } />
-      <ReferenceInput label="Událost" source="event" reference="events">
-        <SelectInput optionText="name" />
-      </ReferenceInput>
-      <NumberInput label='Limit' source='limit' validate={ required() }/>
-      <ColorInput label='Barva' source="color" picker='Sketch' validate={ required() } />
-      <MarkdownInput label='Legenda' source="legend" validate={ required() } />
-      <MarkdownInput label='Požadavky' source="requirements" validate={ required() } />
-      <ImageInput label="Obrázek" source="image">
-        <ImageField source="src" title="title" />
-      </ImageInput>
-    </SimpleForm>
-  </EditBase>
-);
+
+const Edit = (props) => {
+  const classes = useStyles();
+
+  return (
+    <EditBase title={<RaceTitle />} {...props}>
+      <SimpleForm>
+        <TextInput label='Název' source="name" validate={required()} formClassName={classes.inlineBlock} />
+        <ReferenceInput label="Událost" source="event" reference="events" formClassName={classes.inlineBlock} >
+          <SelectInput optionText="name" />
+        </ReferenceInput>
+        <NumberInput label='Limit' source='limit' validate={required()} formClassName={classes.inlineBlock} />
+        <ColorInput label='Barva' source="color" picker='Sketch' validate={required()} />
+        <MarkdownInput label='Legenda' source="legend" validate={required()} />
+        <MarkdownInput label='Požadavky' source="requirements" validate={required()} />
+        <ImageInput label="Obrázek" source="image">
+          <ImageField source="src" title="title" />
+        </ImageInput>
+      </SimpleForm>
+    </EditBase>
+  )
+};
 
 export default Edit;
--- apps/admin/src/containers/Race/Show.jsx
+++ apps/admin/src/containers/Race/Show.jsx
@@ -9,21 +9,26 @@ import { ColorField } from 'react-admin-color-input';
 
 import MarkdownField from 'components/MarkdownField';
 import { RaceTitle } from './shared';
+import { useStyles } from '../shared';
 
-const Show = (props) => (
-  <ShowBase title={ <RaceTitle /> } { ...props }>
-    <SimpleForm>
-      <TextField label='Název' source="name" validate={ required() } />
-      <ReferenceField label="Událost" source="event" reference="events">
-        <TextField optionText="name" />
-      </ReferenceField>
-      <NumberField label='Limit' source='limit' validate={ required() }/>
-      <ColorField label='Barva' source="color" picker='Sketch' validate={ required() } />
-      <MarkdownField label='Legenda' source="legend" validate={ required() } />
-      <MarkdownField label='Požadavky' source="requirements" validate={ required() } />
-      <ImageField source="src" title="title" />
-    </SimpleForm>
-  </ShowBase>
-);
+const Show = (props) => {
+  const classes = useStyles();
+
+  return (
+    <ShowBase title={<RaceTitle />} {...props}>
+      <SimpleForm>
+        <TextField label='Název' source="name" validate={required()} formClassName={classes.inlineBlock} />
+        <ReferenceField label="Událost" source="event" reference="events" formClassName={classes.inlineBlock}>
+          <TextField source="name"  formClassName={classes.inlineBlock}/>
+        </ReferenceField>
+        <NumberField label='Limit' source='limit' validate={required()}  formClassName={classes.inlineBlock}/>
+        <ColorField label='Barva' source="color" picker='Sketch' validate={required()}  formClassName={classes.inlineBlock}/>
+        <MarkdownField label='Legenda' source="legend" validate={required()} />
+        <MarkdownField label='Požadavky' source="requirements" validate={required()} />
+        <ImageField source="image.src" title="image.title" />
+      </SimpleForm>
+    </ShowBase>
+  )
+};
 
 export default Show;
