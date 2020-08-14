3a597106dc1e27fae8a149e17c721e7b4dda6eb6
Tomas Coufal <tcoufal@redhat.com>
2020-08-14 23:02:50 +0200

    Use refactored Article in Contacts

--- apps/public/src/containers/Contact/List.jsx
+++ apps/public/src/containers/Contact/List.jsx
@@ -1,121 +1,65 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import clsx from 'clsx';
 
-import { Grid, Fab, Icon, Typography } from '@material-ui/core';
-import { blue, green, brown, red } from '@material-ui/core/colors';
-import { makeStyles } from '@material-ui/core/styles';
+import { Grid, CardContent, Typography, Divider, Box, Button, Icon } from '@material-ui/core';
+import { TodayOutlined, EventOutlined, MailOutlined } from '@material-ui/icons';
 
-import { Article, ArticleContent, ArticleMedia, Markdown } from 'components';
+import { Article, ArticleCardHeader, Markdown } from 'components';
 
-const useStyles = makeStyles(() => ({
-  blue: {
-    backgroundColor: blue.A400,
-    '&:hover': {
-      backgroundColor: blue.A700
-    }
+const contactButtons = (event) => ([
+  {
+    href: event.contact && event.contact.facebook || '',
+    startIcon: <Icon className='fab fa-facebook-f'/>,
+    text: 'Událost na facebooku'
   },
-  green: {
-    backgroundColor: green[700],
-    '&:hover': {
-      backgroundColor: green[900]
-    }
+  {
+    href: event.contact && event.contact.larpovadatabaze || '',
+    startIcon: <TodayOutlined />,
+    text: 'Larpová Databáze'
   },
-  brown: {
-    backgroundColor: brown[700],
-    '&:hover': {
-      backgroundColor: brown[900]
-    }
+  {
+    href: event.contact && event.contact.larpcz || '',
+    startIcon: <EventOutlined />,
+    text: 'LARP.cz'
   },
-  red: {
-    backgroundColor: red.A400,
-    '&:hover': {
-      backgroundColor: red.A700
-    }
-  },
-  fab: {
-    color: '#fff',
-    margin: '20px 40px',
-    textAlign: 'center'
-  },
-  contactItem: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    margin: 20,
-    minWidth: 200
-  },
-  image: {
-    height: 400,
-    width: '100%',
-    objectFit: 'cover',
-    marginTop: '2em',
-    marginBottom: '2em'
+  {
+    href: event.contact && event.contact.email || '',
+    startIcon: <MailOutlined />,
+    text: 'E-mail organizátorům'
   }
-}));
+]);
 
-const List = ({ event }) => {
-  const classes = useStyles();
-
-  return (
-    <Article>
-      <ArticleContent><Typography gutterBottom variant='h4' component='h2'>Kontakty</Typography></ArticleContent>
-      <ArticleMedia src={ event.contactImage && event.contactImage.src } />
-      <ArticleContent><Markdown content={ event.contactText } /></ArticleContent>
-      <Grid container justify='center'>
-        <div className={ classes.contactItem }>
-          <Fab
-            className={ clsx(`${classes.blue} ${classes.fab}`) }
-            href={ event.contact && event.contact.facebook }
-            target='_blank'
-          >
-            <Icon className='fab fa-facebook-f'/>
-          </Fab>
-          <Typography variant='button' display='block'>
-            Událost na facebooku
-          </Typography>
-        </div>
-        <div className={ classes.contactItem }>
-          <Fab
-            className={ clsx(`${classes.green} ${classes.fab}`) }
-            href={ event.contact && event.contact.larpovadatabaze }
-            target='_blank'
-          >
-            <Icon>today</Icon>
-          </Fab>
-          <Typography variant='button' display='block'>
-            Larpová Databáze
-          </Typography>
-        </div>
-        <div className={ classes.contactItem }>
-          <Fab
-            className={ clsx(`${classes.brown} ${classes.fab}`) }
-            href={ event.contact && event.contact.larpcz }
-            target='_blank'
-          >
-            <Icon>event</Icon>
-          </Fab>
-          <Typography variant='button' display='block'>
-            LARP.cz
-          </Typography>
-        </div>
-        <div className={ classes.contactItem }>
-          <Fab
-            className={ clsx(`${classes.red} ${classes.fab}`) }
-            href={ event.contact && event.contact.email }
-            target='_blank'
-          >
-            <Icon>mail</Icon>
-          </Fab>
-          <Typography variant='button' display='block'>
-            E-mail
-          </Typography>
-        </div>
-      </Grid>
-    </Article>
-  );
-};
+const List = ({ event }) => (
+  <Article>
+    <ArticleCardHeader height={ 600 } title="Kontakty a organizační tým" image={ event.contactImage && event.contactImage.src }/>
+    <CardContent>
+      <Typography variant='h5' gutterBottom component='h3'>O nás</Typography>
+      <Markdown content={ event.contactText } />
+      <Box marginY={ 4 }>
+        <Divider/>
+      </Box>
+      <Typography variant='h5' gutterBottom component='h3'>Kontakty a odkazy pro současný ročník</Typography>
+      <Box marginTop={ 4 }>
+        <Grid container justify='center' spacing={ 2 } >
+          { contactButtons(event).map(c => (
+            <Grid item key={ c.href }>
+              <Button
+                variant="outlined"
+                color="primary"
+                target='_blank'
+                href={ c.href }
+                disabled={ !c.href }
+                startIcon={ c.startIcon }>
+                {c.text}
+              </Button>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </CardContent>
+  </Article>
+);
 
 List.propTypes = {
   event: PropTypes.shape({
