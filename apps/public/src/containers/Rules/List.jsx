2f8b01c584d799644789bded9b0dc11245be8e78
Tomas Coufal <tcoufal@redhat.com>
2020-08-14 22:10:46 +0200

    Use refactored Article in Rules

--- apps/public/src/containers/Rules/List.jsx
+++ apps/public/src/containers/Rules/List.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { Typography } from '@material-ui/core';
+import { CardContent } from '@material-ui/core';
 
-import { Article, ArticleContent, ArticleMedia, Markdown } from 'components';
+import { Article, ArticleCardHeader, Markdown } from 'components';
 
 const List = ({ event }) => (
-  <Article isLoading={ !event }>
-    <ArticleContent>
-      <Typography gutterBottom variant='h4' component='h2'>Pravidla: { event.name } { event.year }</Typography>
-    </ArticleContent>
-    <ArticleMedia src={ event.rules_image && event.rules_image.src } />
-    <ArticleContent><Markdown content={ event.rules } /></ArticleContent>
+  <Article>
+    <ArticleCardHeader title={ `${event.name} - Pravidla` } image={ event.rules_image && event.rules_image.src }/>
+    <CardContent>
+      <Markdown content={ event.rules } />
+    </CardContent>
   </Article>
 );
 
