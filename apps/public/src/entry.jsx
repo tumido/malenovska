0d878f521f8acbc1c2b1e5b73800721ce12924a6
Tomas Coufal <tcoufal@redhat.com>
2020-08-21 16:18:43 +0200

    Trying to add react-snap

--- apps/public/src/entry.jsx
+++ apps/public/src/entry.jsx
@@ -1,9 +1,9 @@
 // Polyfills
-import 'whatwg-fetch';
+// import 'whatwg-fetch';
 import('smoothscroll-polyfill').then(smoothscroll => smoothscroll.polyfill());
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
@@ -21,27 +21,35 @@ const store = configureStore();
 
 const IE = process.browser && /MSIE|Trident/.test(navigator.userAgent);
 
-const render = () => {
-  ReactDOM.render(
-    <Provider store={ store }>
-      <ReactReduxFirebaseProvider { ...rrfProps(store) }>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ReactReduxFirebaseProvider>
-    </Provider>,
-    document.getElementById('app')
-  );
+const rootElement = document.getElementById('app');
+
+const WrappedApp = () => {
   if (IE) {
     alert(`
       Internet Explorer není podporován. Stránka se nemusí správně zobrazit.
       Použijte prosím cokoliv normálního. Například Firefox, Chrome či Edge.
     `);
   }
+
+  return (
+    <Provider store={ store }>
+      <ReactReduxFirebaseProvider { ...rrfProps(store) }>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ReactReduxFirebaseProvider>
+    </Provider>
+  );
 };
 
 if (module.hot) {
   enableFirebasePersistence();
+}
+
+if (rootElement.hasChildNodes()) {
+  hydrate(<WrappedApp />, rootElement);
+} else {
+  render(<WrappedApp />, rootElement);
 }
 
 render();
