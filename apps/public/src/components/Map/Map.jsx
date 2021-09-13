8d1bb44f1f417d923b2831d749f2f232a2f483d7
Tomas Coufal <tcoufal@redhat.com>
2021-09-13 18:56:11 +0200

    fix: Map centering
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- apps/public/src/components/Map/Map.jsx
+++ apps/public/src/components/Map/Map.jsx
@@ -1,87 +1,105 @@
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import React, { useEffect } from "react";
+import { renderToString } from "react-dom/server";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
 
-import { latLngBounds, divIcon } from 'leaflet';
-import { Grid, Icon } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { latLngBounds, divIcon } from "leaflet";
+import { Grid, Icon } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-import 'leaflet/dist/leaflet.css';
+import "leaflet/dist/leaflet.css";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    height: '100%'
+    height: "100%",
   },
   map: {
     minHeight: 400,
     flexGrow: 1,
-    display: 'flex',
-    '& .leaflet-marker-icon': {
-      filter: 'drop-shadow(8px 8px 8px #000)'
+    display: "flex",
+    "& .leaflet-marker-icon": {
+      filter: "drop-shadow(8px 8px 8px #000)",
     },
-    '& .leaflet-pane': {
-      zIndex: 0
+    "& .leaflet-pane": {
+      zIndex: 0,
     },
-    '& .leaflet-popup-content-wrapper': {
-      borderRadius: 0
+    "& .leaflet-popup-content-wrapper": {
+      borderRadius: 0,
     },
-    '& .leaflet-popup-content': {
-      margin: '1rem',
-      fontSize: '1rem',
-      width: '100%'
-    }
+    "& .leaflet-popup-content": {
+      margin: "1rem",
+      fontSize: "1rem",
+      width: "100%",
+    },
   },
   color0: {
-    color: theme.palette.loading[0]
+    color: theme.palette.loading[0],
   },
   color1: {
-    color: theme.palette.loading[1]
+    color: theme.palette.loading[1],
   },
   color2: {
-    color: theme.palette.loading[2]
+    color: theme.palette.loading[2],
   },
   maximize: {
     flexGrow: 1,
-    width: '100%'
-  }
+    width: "100%",
+  },
 }));
+
+const Centerer = ({ center }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (center) {
+      map.setView(center);
+    }
+  }, [center, map]);
+
+  return null;
+};
 
 const Map = ({ markers, center, className }) => {
   const classes = useStyles();
 
   const markersToRender = markers.map((marker, index) => (
     <Marker
-      key={ `point_${index}` }
-      position={ [ marker.latitude, marker.longitude ] }
-      icon={ divIcon({
-        html: renderToString(<Icon fontSize='large' className={ classes[`color${index % 3}`] }>location_on</Icon>),
-        className: ''
-      }) }
+      key={`point_${index}`}
+      position={[marker.latitude, marker.longitude]}
+      icon={divIcon({
+        html: renderToString(
+          <Icon fontSize="large" className={classes[`color${index % 3}`]}>
+            location_on
+          </Icon>
+        ),
+        className: "",
+      })}
     />
   ));
+  console.log(center);
 
   return (
-    <Grid container className={ clsx(`${classes.root} ${className}`) }>
-      <Grid item className={ classes.map }>
+    <Grid container className={clsx(`${classes.root} ${className}`)}>
+      <Grid item className={classes.map}>
         <MapContainer
-          center={ center ||
-            latLngBounds(
-              markers.map(marker => [ marker.latitude, marker.longitude ])
-            ).getCenter() }
-          zoom={ 15 }
-          className={ classes.maximize }>
+          center={latLngBounds(
+            markers.map((marker) => [marker.latitude, marker.longitude])
+          ).getCenter()}
+          zoom={15}
+          className={classes.maximize}
+        >
+          <Centerer center={center} />
           <TileLayer
             url="http://tile.mtbmap.cz/mtbmap_tiles/{z}/{x}/{y}.png"
-            attribution={ renderToString(
+            attribution={renderToString(
               <React.Fragment>
                 <a href="http://osm.org/copyright"> OpenStreetMap</a> and
                 <a href="http://www.mtbmap.cz">MTB map Europe</a>
               </React.Fragment>
-            ) }
+            )}
           />
-          { markersToRender }
+          {markersToRender}
         </MapContainer>
       </Grid>
     </Grid>
@@ -91,7 +109,7 @@ const Map = ({ markers, center, className }) => {
 Map.propTypes = {
   markers: PropTypes.array,
   className: PropTypes.string,
-  center: PropTypes.array
+  center: PropTypes.array,
 };
 
 export default Map;
